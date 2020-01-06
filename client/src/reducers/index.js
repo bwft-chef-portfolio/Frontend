f84871aacc4e9f463989b94b3f6216a3b18fde84
Ethan Hoover <51389138+Cireimu@users.noreply.github.com>
2020-01-06 12:27:27 -0800

    added card and chefPage reducers to reducer index

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 import {registrationReducer as registration} from './registrationReducer';
 import { loginReducer} from './loginReducer';
+import { chefPageReducer } from './chefPageReducer';
+import { cardReducer } from './cardReducer';
 
 export default combineReducers({
-    registration, loginReducer
+    registration, loginReducer, chefPageReducer, cardReducer
+
 });
