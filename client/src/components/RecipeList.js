1ab57111dd6caafd73faf5367be48fee62cb0b4c
Celestia Salinas <csalinas3671@gmail.com>
2020-01-07 16:05:41 -0500

    RecipleList created

---
+++ client/src/components/RecipeList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './Cards/Card'
+
+const RecipeList = props => {
+    return (
+        <div>
+            <Card />
+        </div>
+    )
+}
+
+export default RecipeList
